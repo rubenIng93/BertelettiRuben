90944b2d095b8726eb73d2027bcfb5496503f998
ruben <ruben@LAPTOP-OGB4QBF6>
2019-08-10 19:17:33 +0200

    Primo approccio grafico

---
+++ Tesi-Simulatore_NBA/src/it/polito/tdp/simulatoreNBA/controller/NBAController.java
@@ -0,0 +1,84 @@
+package it.polito.tdp.simulatoreNBA.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class NBAController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest1;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest8;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest4;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest5;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest3;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest6;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest2;
+
+    @FXML
+    private ComboBox<?> cmbBoxWest7;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast1;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast8;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast4;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast5;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast3;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast6;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast2;
+
+    @FXML
+    private ComboBox<?> cmbBoxEast7;
+
+    @FXML
+    void initialize() {
+        assert cmbBoxWest1 != null : "fx:id=\"cmbBoxWest1\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxWest8 != null : "fx:id=\"cmbBoxWest8\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxWest4 != null : "fx:id=\"cmbBoxWest4\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxWest5 != null : "fx:id=\"cmbBoxWest5\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxWest3 != null : "fx:id=\"cmbBoxWest3\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxWest6 != null : "fx:id=\"cmbBoxWest6\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxWest2 != null : "fx:id=\"cmbBoxWest2\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxWest7 != null : "fx:id=\"cmbBoxWest7\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast1 != null : "fx:id=\"cmbBoxEast1\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast8 != null : "fx:id=\"cmbBoxEast8\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast4 != null : "fx:id=\"cmbBoxEast4\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast5 != null : "fx:id=\"cmbBoxEast5\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast3 != null : "fx:id=\"cmbBoxEast3\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast6 != null : "fx:id=\"cmbBoxEast6\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast2 != null : "fx:id=\"cmbBoxEast2\" was not injected: check your FXML file 'Table.fxml'.";
+        assert cmbBoxEast7 != null : "fx:id=\"cmbBoxEast7\" was not injected: check your FXML file 'Table.fxml'.";
+
+    }
+}
---
+++ Tesi-Simulatore_NBA/src/it/polito/tdp/simulatoreNBA/model/Evento.java
@@ -0,0 +1,5 @@
+package it.polito.tdp.simulatoreNBA.model;
+
+public class Evento {
+
+}
