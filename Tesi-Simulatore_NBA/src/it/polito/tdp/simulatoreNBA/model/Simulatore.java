06d1b32a2e6566737730efde4d7c71b68354572e
ruben <ruben@LAPTOP-OGB4QBF6>
2019-08-14 19:11:12 +0200

    Proseguo inizializzazione simulatore

--- Tesi-Simulatore_NBA/src/it/polito/tdp/simulatoreNBA/model/Simulatore.java
+++ Tesi-Simulatore_NBA/src/it/polito/tdp/simulatoreNBA/model/Simulatore.java
@@ -1,8 +1,11 @@
 package it.polito.tdp.simulatoreNBA.model;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Random;
+import java.util.stream.DoubleStream;
 
 import it.polito.tdp.simulatoreNBA.db.NBADao;
 
@@ -12,6 +15,9 @@ public class Simulatore {
 	private Random rand;
 	private Match match;
 	private NBADao dao;
+	
+	//Parametri simulazione
+	private final Duration matchDurations = Duration.ofMinutes(48);
 	
 	
 	
@@ -28,6 +34,30 @@ public class Simulatore {
 		
 		this.match.setAway(away);
 		this.match.setHome(home);
+		
+		LocalTime matchTime = LocalTime.of(0, 0, 0);
+		//Durata partita 48 minuti
+		while(matchTime.isBefore(matchTime.plus(matchDurations))) {
+			
+			/*
+			 * Metto in coda un'azione in un periodo casuale tra 30s e 60s
+			 */
+			matchTime = matchTime.plusSeconds((rand.nextInt(30)) + 30);
+			
+			for(Player homeP : hPlayers) {
+				/*
+				 * Ogni giocatore prende tiri in base alla media della regular season
+				 * impostato attualmente al tra il 70% e 100% in modo randomico
+				 */
+				Integer attemtp3 = (int) ((((rand.nextInt(30) + 70) * homeP.getThreePointsAttempts()) / 100));
+				Integer attempt2 =
+				Integer freeT = 
+				
+			}
+			
+		}
+		
+		
 		
 		
 	}
